d499607287f8871c867f7112e1d44d04607d3a16
maulana saputra <maulana.saputra@aqi.co.id>
2021-09-16 10:22:20 +0700

    update style shop label
    Signed-off-by: maulana saputra <maulana.saputra@aqi.co.id>

--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.db.models import Count
 
 from account.models import UserBase
 
 def chat_inbox(request):
+    if hasattr(request.user, 'shop') == False:
+        return redirect('account:dashboard')
+    
     users = []
     for user in request.user.comments_to.all().values('user').annotate(total=Count('id')).order_by():
         users.append(UserBase.objects.get(id=user['user']))
@@ -11,6 +14,8 @@ def chat_inbox(request):
     return render(request, 'chat/home.html', {'users': users})
 
 def chat_sent(request):
+    if hasattr(request.user, 'shop') == True:
+        return redirect('account:dashboard')
     
     users = []
     for recipient in request.user.comments_from.all().values('recipient').annotate(total=Count('id')).order_by():
